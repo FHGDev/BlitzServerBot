d8d3d8856a4976712249fded5a2ac2b76fc4ffce
BlitzProducts <44275189+BlitzProducts@users.noreply.github.com>
2018-12-07 16:12:30 +0000

    Delete setnormal.js

--- commands/setnormal.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = (bot, message, args) => {
-    if (!bot.owners.includes(message.author.id)) return;
-    bot.user.setActivity(`for b!help | ${bot.guilds.size} servers`, {type: "WATCHING"})
-    message.channel.send("I set my activity back to normal.")
-}
-
-module.exports.help = {name: "setnormal"}
